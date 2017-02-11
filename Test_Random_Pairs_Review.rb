533b02511b207aa38d32d84c562626422077eca7
TMerrick55 <tmerrick44@gmail.com>
2017-02-11 13:53:29 -0500

    Random Pairs Review Ver. 2.0: RWBY Teams

--- Test_Random_Pairs_Review.rb
+++ Test_Random_Pairs_Review.rb
@@ -3,8 +3,18 @@ require_relative 'Random_Pairs_Review.rb'
 
 class TestPairsArray < Minitest::Test
 
-	def test_team_RWBY_is_two_pairs()
+	def test_team_RWBY()
 		pairs = create_partners(['Ruby', 'Weiss', 'Blake', 'Yang'])
 		assert_equal(2, pairs.count)
+	end
+
+	def test_teams_RWBY_and_JNPR()
+		pairs = create_partners(['Ruby', 'Weiss', 'Blake', 'Yang', 'Jaune', 'Nora', 'Pyrrha', 'Ren'])
+		assert_equal(4, pairs.count)
+	end
+
+	def test_teams_RWBY_JNPR_and_CFVY
+		pairs = create_partners(['Ruby', 'Weiss', 'Blake', 'Yang', 'Jaune', 'Nora', 'Pyrrha', 'Ren', 'Coco', 'Fox', 'Velvet', 'Yatsu'])
+		assert_equal(6, pairs.count)
 	end
 end
